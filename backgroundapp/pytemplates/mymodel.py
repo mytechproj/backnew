03747f76b787fb73dcdf69a107b5934c45bf0741
ssumasudhan <33372118+ssumasudhan@users.noreply.github.com>
2021-01-07 13:48:18 +0530

    Update mymodel.py

--- backgroundapp/pytemplates/mymodel.py
+++ backgroundapp/pytemplates/mymodel.py
@@ -1,8 +1,101 @@
 import os
+from skimage import io, transform
+import torch
+import torchvision
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms#, utils
+#from tensorflow.keras.preprocessing.image import load_img
+#from tensorflow.keras.preprocessing.image import img_to_array
+# import torch.optim as optim
 
-def main(imgname):   
-    print("here1")    
-    #return 'test.png'
+import numpy as np
+from PIL import Image
+import glob
+#import cv2
+
+from .data_loader import RescaleT
+from .data_loader import ToTensor
+from .data_loader import ToTensorLab
+from .data_loader import SalObjDataset
+
+from .model import U2NET # full size version 173.6 MB
+from .model import U2NETP # small version u2net 4.7 MB
+
+# normalize the predicted SOD probability map
+def normPRED(d):
+    ma = torch.max(d)
+    mi = torch.min(d)
+
+    dn = (d-mi)/(ma-mi)
+
+    return dn
+
+def save_output(image_name,pred,d_dir):
+
+    predict = pred
+    predict = predict.squeeze()
+    predict_np = predict.cpu().data.numpy()
+
+    im = Image.fromarray(predict_np*255).convert('RGB')
+    img_name = image_name.split(os.sep)[-1]    
+    image = io.imread(image_name)
+    imo = im.resize((image.shape[1],image.shape[0]),resample=Image.BILINEAR)
+
+    pb_np = np.array(imo)
+
+    aaa = img_name.split(".")
+    bbb = aaa[0:-1]
+    imidx = bbb[0]
+    for i in range(1,len(bbb)):
+        imidx = imidx + "." + bbb[i]
+
+    imo.save(d_dir+imidx+'.png')
+def fnfilepath(strpath):
+    return strpath
+def main(imgname):
+
+    # --------- 1. get image path and name ---------
+    model_name='u2netp'# change to u2netp
+    #imgname = "dog1.jpg"
+    image_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'pytemplates','images',imgname) # changed to 'images' directory which is populated while running the script
+    #image_dir = "E:\\DjangoDeepLearning\\djangotutorial\\static\\img" # changed to 'images' directory which is populated while running the script
+    prediction_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'pytemplates','results')+ os.sep # changed to 'results' directory which is populated after the predictions
+    #prediction_dir = "E:\\DjangoDeepLearning\\djangotutorial\\static\\img" # changed to 'results' directory which is populated after the predictions
+    model_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)),
+	'pytemplates','u2netp.pth') # path to u2netp pretrained weights
+
+    #img_name_list = glob.glob(image_dir + os.sep + '*')      
+    img_name_list = glob.glob(image_dir)   
+    print("here1111")
+    # --------- 2. dataloader ---------
+    #1. dataloader
+    
+    test_salobj_dataset = SalObjDataset(img_name_list = img_name_list,
+                                        lbl_name_list = [],
+                                        transform=transforms.Compose([RescaleT(320),
+                                                                      ToTensorLab(flag=0)])
+                                        )    
+        
+    test_salobj_dataloader = DataLoader(test_salobj_dataset,
+                                        batch_size=1,
+                                        shuffle=False,
+                                        num_workers=0)
+    print("here2")
+    # --------- 3. model define ---------
+    net = U2NETP(3,1)
+    
+    if torch.cuda.is_available():        
+        net.load_state_dict(torch.load(model_dir))
+        net.cuda()
+        net.eval()
+    else:        
+        net.load_state_dict(torch.load(model_dir,map_location="cpu"))
+        net.eval()        
+    print("here3")
+    # --------- 4. inference for each image ---------   
         
    
     
