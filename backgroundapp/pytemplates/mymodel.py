6d50a6012bb5dd6be461b19d77e575802d716fa9
ssumasudhan <33372118+ssumasudhan@users.noreply.github.com>
2021-01-07 13:09:38 +0530

    Update mymodel.py

--- backgroundapp/pytemplates/mymodel.py
+++ backgroundapp/pytemplates/mymodel.py
@@ -70,60 +70,8 @@ def main(imgname):
 
     #img_name_list = glob.glob(image_dir + os.sep + '*')      
     img_name_list = glob.glob(image_dir)   
-    print("here1")
-    # --------- 2. dataloader ---------
-    #1. dataloader
-    
-    test_salobj_dataset = SalObjDataset(img_name_list = img_name_list,
-                                        lbl_name_list = [],
-                                        transform=transforms.Compose([RescaleT(320),
-                                                                      ToTensorLab(flag=0)])
-                                        )    
-        
-    test_salobj_dataloader = DataLoader(test_salobj_dataset,
-                                        batch_size=1,
-                                        shuffle=False,
-                                        num_workers=0)
-    print("here2")
-    # --------- 3. model define ---------
-    net = U2NETP(3,1)
-    
-    if torch.cuda.is_available():        
-        net.load_state_dict(torch.load(model_dir))
-        net.cuda()
-        net.eval()
-    else:        
-        net.load_state_dict(torch.load(model_dir,map_location="cpu"))
-        net.eval()        
-    print("here3")
-   
-    # --------- 4. inference for each image ---------
-    for i_test, data_test in enumerate(test_salobj_dataloader):                
-        print("inferencing:",img_name_list[i_test].split(os.sep)[-1])        
-
-        inputs_test = data_test['image']        
-        inputs_test = inputs_test.type(torch.FloatTensor)
-
-        if torch.cuda.is_available():
-            inputs_test = Variable(inputs_test.cuda())
-        else:
-            inputs_test = Variable(inputs_test)
-
-        d1,d2,d3,d4,d5,d6,d7= net(inputs_test)
-
-        # normalization
-        pred = d1[:,0,:,:]
-        pred = normPRED(pred)
-        
-        # save results to test_results folder
-        if not os.path.exists(prediction_dir):
-            os.makedirs(prediction_dir, exist_ok=True)
-        save_output(img_name_list[i_test],pred,prediction_dir)
-
-        del d1,d2,d3,d4,d5,d6,d7
-	print("here4")
-   
-        return 'test.png'
+    print("here1")    
+    return 'test.png'
         
    
     
